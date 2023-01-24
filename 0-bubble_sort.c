fa4deeae148f866104033039b1afa13214c00935
Sue-wesh <shiroweru007@gmail.com>
2023-01-24 09:24:40 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,13 +29,13 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	
+
 	while (check == false)
 	{
 		check = true;
 		for (i = 0; i < (size - 1); i++)
 		{
-			if (array[i] > array[i+1])
+			if (array[i] > array[i + 1])
 			{
 				swap(array + i, array + i + 1);
 				print_array(array, size);
